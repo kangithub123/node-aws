309112d1581fe64ba1883ed70aacb33e2875451a
Kanishka Rajapakse <krajapakse95@gmailcom>
2020-09-26 16:51:27 +0530

    port change

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var app = require('./app');
-var server = app.listen(3000, function(){
+const port = process.env.port || 3000;
+var server = app.listen(port, function(){
      console.log("Server is running on port 3000");
 });
